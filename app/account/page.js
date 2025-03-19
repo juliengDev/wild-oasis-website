5bc713fdf619a8c1337fbcf0b9060019c98779c9
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-19 12:18:57 +0700

    adding metadata and favicon

--- app/account/page.js
+++ app/account/page.js
@@ -1,4 +1,6 @@
-
+export const metadata = {
+  title: "Account",
+};
 export default function Page() {
   return (
     <div>
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the hearft of the Italian Dolomites, surrounded by beautiful mountains and dark forest",
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen bg-primary-950 text-primary-100">
         <header>
           <Logo />
         </header>
