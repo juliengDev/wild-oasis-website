d57123223ab3e72bc5cf2f1e44f536b745491ba8
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-19 15:02:05 +0700

    building the home page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -22,14 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${josefin.className} min-h-screen bg-primary-950 text-primary-100`}
+        className={` ${josefin.className} flex min-h-screen flex-col bg-primary-950 text-primary-100`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
