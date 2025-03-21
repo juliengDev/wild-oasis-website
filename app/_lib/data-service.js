8fa8607c8d86f29ef2f5ce2ee2a4bb1731d8836d
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-21 16:49:46 +0700

    reservation section build

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
+import { notFound } from "next/navigation";
 /////////////
 // GET
 
@@ -15,6 +16,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
@@ -120,6 +122,8 @@ export async function getBookedDatesByCabinId(cabinId) {
     })
     .flat();
 
+  // for testing
+  // await new Promise((res) => setTimeout(res, 5000));
   return bookedDates;
 }
 
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,19 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
+
+export const revalidate = 3600;
+/* in seconds, used for static page only, by using searchParams on the page component
+you turn that component page into a dynamic page */
 
 export const metadata = {
   title: "Cabins",
 };
-export default async function Page() {
+
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="mb-5 text-4xl font-medium text-accent-400">
@@ -15,12 +23,15 @@ export default async function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature's beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="mb-8 flex justify-end">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
