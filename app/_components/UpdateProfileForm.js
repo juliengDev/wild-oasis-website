6015e9105306e3ee27ac911e48652772a37bc179
Julien Gilbert <juliengilbert.dev@gmail.com>
2025-03-24 16:05:23 +0700

    creating server action to mutate profil data with updateGuest()

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,18 +1,25 @@
 "use client";
-
+import { useFormStatus } from "react-dom";
 import { useState } from "react";
 import Image from "next/image";
+import { updateGuest } from "../_lib/actions";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+  // console.log(guest);
+
   return (
-    <form className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg">
+    <form
+      action={updateGuest}
+      className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
           disabled
+          defaultValue={fullName}
+          name="fullName"
           className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
       </div>
@@ -20,6 +27,8 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <label>Email address</label>
         <input
+          defaultValue={email}
+          name="email"
           disabled
           className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -28,11 +37,16 @@ function UpdateProfileForm({ children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <Image
-            src={countryFlag}
-            alt="Country flag"
-            className="h-5 rounded-sm"
-          />
+          <div className="relative flex items-center">
+            <Image
+              width={30}
+              height={30}
+              src={countryFlag}
+              alt="Country flag"
+              name="countryFlag"
+              className="ml-4 h-5 flex-1 rounded-sm object-cover"
+            />
+          </div>
         </div>
         {children}
       </div>
@@ -41,16 +55,27 @@ function UpdateProfileForm({ children }) {
         <label htmlFor="nationalID">National ID number</label>
         <input
           name="nationalID"
+          defaultValue={nationalID}
           className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
         />
       </div>
 
       <div className="flex items-center justify-end gap-6">
-        <button className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const status = useFormStatus();
+  return (
+    <button
+      disabled={status.pending}
+      className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {status.pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
 
--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -180,18 +180,21 @@ export async function createBooking(newBooking) {
   return data;
 }
 
+
 /////////////
 // UPDATE
 
+
+/* 
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
-    .from("guests")
-    .update(updatedFields)
-    .eq("id", id)
-    .select()
-    .single();
-
+  .from("guests")
+  .update(updatedFields)
+  .eq("id", id)
+  .select()
+  .single();
+  
   if (error) {
     console.error(error);
     throw new Error("Guest could not be updated");
@@ -201,28 +204,30 @@ export async function updateGuest(id, updatedFields) {
 
 export async function updateBooking(id, updatedFields) {
   const { data, error } = await supabase
-    .from("bookings")
-    .update(updatedFields)
+  .from("bookings")
+  .update(updatedFields)
     .eq("id", id)
     .select()
     .single();
-
-  if (error) {
-    console.error(error);
-    throw new Error("Booking could not be updated");
+    
+    if (error) {
+      console.error(error);
+      throw new Error("Booking could not be updated");
+    }
+    return data;
   }
-  return data;
-}
-
-/////////////
-// DELETE
-
-export async function deleteBooking(id) {
-  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
-
-  if (error) {
-    console.error(error);
-    throw new Error("Booking could not be deleted");
+  
+  /////////////
+  // DELETE
+  
+  export async function deleteBooking(id) {
+    const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+    
+    if (error) {
+      console.error(error);
+      throw new Error("Booking could not be deleted");
+    }
+    return data;
   }
-  return data;
-}
+  
+  */
